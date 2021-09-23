3e08f104805a6c3b9ecbe338bb1e0243528ab423
Troy Hayman <THayman@innout.com>
2021-09-23 07:42:47 -0700

    css fix and li created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent implements OnInit {
+  product = {
+    name:'Thingamajiggy',
+    price: 179.99,
+    img_url: 'https://images.unsplash.com/photo-1591076482161-42ce6da69f67?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'
+  };
 
   constructor() { }
 
